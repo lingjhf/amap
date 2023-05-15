b5215aa46ef8b4152da40f3811000beb1b8a396c
fengjianhao <lingj.jhf@outlook.com>
2023-05-15 14:39:55 +0800

    添加类型

--- src/components/type.ts
+++ src/components/type.ts
@@ -4,8 +4,11 @@ import '@amap/amap-jsapi-types'
 type AMapType = (typeof AMap)
 type Map = AMap.Map
 type LngLatLike = AMap.LngLatLike
+type Polygon = AMap.Polygon
+type Polyline = AMap.Polyline
 type PolygonOptions = AMap.PolygonOptions
-type PolygonCallback =  (polygon: AMap.Polygon) => void
+type PolygonCallback = (polygon: AMap.Polygon) => void
+type PolylineOptions = AMap.PolylineOptions
+type PolylineCallback = (polyline: AMap.Polyline) => void
 
-
-export type { AMapType, Map, LngLatLike, PolygonOptions,PolygonCallback }
+export type { AMapType, Map, LngLatLike, PolygonOptions, PolygonCallback, PolylineOptions, PolylineCallback, Polygon, Polyline }
